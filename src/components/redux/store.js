b8471a97dc77ede46bea20f0b9186f43eba269e7
zalvladik <zalipkav@ukr.net>
2023-05-21 23:46:20 +0300

    kk

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,11 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { contactsReducer } from './contactsReducer'
-import { filterListReducer } from './filterListReducer'
 
-export const store = configureStore({
-    reducer:{
-        contacts:contactsReducer,
-        filter:filterListReducer,
-    }
-})
+import { persistStore, persistReducer } from "redux-persist";
+import storage from 'redux-persist/lib/storage'
 
+import { reducer } from './reducer'
+
+const persistConfig = {
+    key:'contactsList',
+    storage,
+    whitelist : ['contacts']
+}
+
+const persistedReducer = persistReducer( persistConfig, reducer )
+
+export const store = configureStore({ reducer: persistedReducer })
+
+export const persistor = persistStore( store ) 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App';
 import './index.css';
-import {store} from './components/redux/store'
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './components/redux/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
